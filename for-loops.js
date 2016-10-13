ed55fd0f6df669b247272ed741d5d189642a7a71
Samuel Phillips <samphillips1879@gmail.com>
2016-10-13 14:17:59 -0500

    first commit

---
+++ for-loops.js
@@ -0,0 +1,12 @@
+
+console.log("addition")
+
+for (var i = 5; i <= 120; i += 10) {
+	console.log("i =", i)
+}
+
+console.log("division")
+
+for (var i = 4096; i > 1; i = i / 2) {
+	console.log ("i =", i)
+}
