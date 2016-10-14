ffea5a15869a6ec43e3954d2fb796a1dfba8a0bc
Samuel Phillips <samphillips1879@gmail.com>
2016-10-14 08:41:30 -0500

    end of day 1

--- for-loops.js
+++ for-loops.js
@@ -1,12 +1,29 @@
 
-console.log("addition")
+console.log("addition");
+
+var i;
 
 for (var i = 5; i <= 120; i += 10) {
-	console.log("i =", i)
+	console.log("i =", i);
 }
 
-console.log("division")
+console.log("division");
 
 for (var i = 4096; i > 1; i = i / 2) {
-	console.log ("i =", i)
+	console.log ("i =", i);
+}
+
+
+
+
+
+
+console.log("array iteration");
+
+
+var prez = ["George Washington", "John Adams", "Thomas Jefferson", "Commander Fluffy"];
+
+
+for (i = 0; i < prez.length; i ++) {
+	console.log("President #" + (i + 1) + " was " + prez[i]);
 }
